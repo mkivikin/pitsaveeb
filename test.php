282d8ff9994f70a84bd893b7c12edbaad61199ac
Marek Kivikink <mkivikin@users.noreply.github.com>
2017-12-10 17:15:38 +0200

    teststyle ja test.php lisatud, pitsaloomise visualiseerimiseks

---
+++ test.php
@@ -0,0 +1,50 @@
+<?php
+	require("../../config.php");
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="styletest.css">
+	<title>Valmista Pitsa</title>
+</head>
+<body>
+<div class="container">
+	<div class="header">
+	</div><!-- end header -->
+	<div class="body">
+	<div class="toppings-outer">
+		<div class="toppings-inner">
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
+		</div><!-- end toppings-inner -->
+	</div><!-- end toppings-outer -->
+	<div class="bottoms-outer">
+		<div class="bottoms-inner">
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
+		</div><!-- end bottoms-inner -->
+	</div><!-- end bottoms-outer -->
+	</div><!-- end body -->
+</div><!-- end container -->
+</body>
+</html>
