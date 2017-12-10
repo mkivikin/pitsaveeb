26b8d84a22da05f804acf3ff332f437555de207a
Marek Kivikink <mkivikin@users.noreply.github.com>
2017-12-10 18:57:33 +0200

    Lisatud toppingute andmebaasist lugemine

--- test.php
+++ test.php
@@ -1,5 +1,36 @@
 <?php
 	require("../../config.php");
+	
+	function laeVeged() {
+	$database = "if17_marek6";
+	$notice = "";
+	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"],$GLOBALS["serverPassword"], $database);
+	$stmt = $mysqli->prepare("SELECT topping_name, topping_price, topping_png FROM toppings WHERE topping_type = 2");
+	$stmt->bind_result($toppingName, $toppingPrice, $toppingpng);
+	$stmt->execute();
+	while ($stmt->fetch()) {
+		//<a href="toppings/". $toppingpng .><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
+		$notice .= '<a href=#' .$toppingName .'><img src ="toppings/' .$toppingpng .'" height="250" /></a>';
+	}
+	$mysqli->close();
+	$stmt->close();
+	return $notice;
+	}
+	function laeLihad() {
+	$database = "if17_marek6";
+	$notice = "";
+	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"],$GLOBALS["serverPassword"], $database);
+	$stmt = $mysqli->prepare("SELECT topping_name, topping_price, topping_png FROM toppings WHERE topping_type = 1");
+	$stmt->bind_result($toppingName, $toppingPrice, $toppingpng);
+	$stmt->execute();
+	while ($stmt->fetch()) {
+		//<a href="toppings/". $toppingpng .><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
+		$notice .= '<a href=#' .$toppingName .'><img src ="toppings/' .$toppingpng .'" height="250" /></a>';
+	}
+	$mysqli->close();
+	$stmt->close();
+	return $notice;
+	}
 ?>
 
 <!DOCTYPE html>
@@ -16,22 +47,19 @@
 	<div class="body">
 	<div class="toppings-outer">
 		<div class="toppings-inner">
-			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
-			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
-			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
-			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
-			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
-			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
-			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
-			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
-			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
-			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
-			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a><br>
+			<?php echo laeLihad();?>
 		</div><!-- end toppings-inner -->
 	</div><!-- end toppings-outer -->
+	<div class="pizza-outer">
+		<div class="pizza-inner">
+		<a href="toppings/bottom.png"><img src="toppings/bottom.png" height="550" /></a>
+		</div><!-- pizza-inner -->
+	</div><!-- pizza-outer -->
 	<div class="bottoms-cont">
 	<div class="bottoms-outer">
 		<div class="bottoms-inner">
+			<?php echo laeVeged();?>
+			<!--<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
 			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
 			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
 			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
@@ -41,8 +69,7 @@
 			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
 			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
 			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
-			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
-			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>
+			<a href="Graafika/pizza_create_pics/meat/sausage.png"><img src="Graafika/pizza_create_pics/meat/sausage.png" height="190" /></a>-->
 		</div><!-- end bottoms-inner -->
 	</div><!-- end bottoms-outer -->
 	</div><!-- end bottoms-cont -->
